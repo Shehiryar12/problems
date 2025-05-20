fbcdbfd67900c793b242393eddedf57331d244d4
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-20 13:59:32 +0500

    learn promise.js

--- promise.js
+++ promise.js
@@ -10,7 +10,6 @@
  name.then((print)=>console.log(print))
  name.catch((notprint)=>console.log(notprint))
 
-
  // resolve tab chalta hai jab ap ka program successfully run ho jaye.
  // reject tab chalta hai jab ap ka program successfully run na ho
  //resolve used with then
