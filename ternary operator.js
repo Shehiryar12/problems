627f32403cce395d23ef5b927f03ae71cb8f65c0
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-21 00:04:58 +0500

    learn ternaryoperator.js

--- ternary operator.js
+++
@@ -1,6 +0,0 @@
- //ternary condition: ternary operator is a shortcut way of if else statement
-
- //syntax:-      (condition)?true expression:false expression
-
- const score=50;
- (score<100)? "score is less than 50":"score is greater than 50"
---
+++ ternaryoperator.js
@@ -0,0 +1,13 @@
+//  //ternary condition: ternary operator is a shortcut way of if else statement
+
+//  //syntax:-      (condition)?true expression:false expression
+
+//  const score=50;
+//  let result=(score<100)? "score is less than 50":"score is greater than 50"
+// console.log(result)
+
+//-------------in home--------------------
+
+ const number=100;
+ let school= (number<=100)? "number is greater than 100":"number is less than 100"
+ console.log(school);
