47259ae7c3625f967c56c11343ad2ca21e01c141
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-20 18:43:04 +0500

    learn destructor.js

---
+++ Destructor.js
@@ -0,0 +1,10 @@
+//Destructor convert object,propertyarray into variables.
+
+const man={
+      firstName:"shehiryar",
+      lastName:"zahid",
+      age:22
+}
+
+let {firstName, lastName} =man;
+console.log(firstName)
