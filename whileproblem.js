502d24a38c8bfbb20c84c955b9f4cd1e75716a19
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-21 18:29:57 +0500

    learn while problem.js

---
+++ whileproblem.js
@@ -0,0 +1,39 @@
+//    print number from 1 to 10
+//  let count = 1;
+// while (count <= 10) {
+//   console.log(count);
+//   count++;
+// }
+
+//------------------------------------------------------------------------------------------
+
+// //Print even numbers from 2 to 20 using a while loop.
+
+// let num=2;
+// while(num<=20){
+//     console.log(num*2)
+//     num++;
+// }
+
+//------------------------------------------------------------------------------------------
+
+// // Print numbers from 10 to 1 (reverse) using a while loop.
+
+// let num=10;
+// while(num>=1){
+//      console.log(num)
+//      num--;   
+// }
+
+//-----------------------------------------------------------------------------------------
+
+// Print all odd numbers between 1 and 15 using a while loop.
+
+// let num=1;
+// while(num<=5){
+//     console.log(num*3)
+//     num++;
+// }
+//-----------------------------------------------------------------------------------------
+
+
