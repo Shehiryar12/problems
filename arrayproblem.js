7caea391318cdd4d690656dfcf8f403a411ee2e5
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-23 15:07:10 +0500

    learn switch.js

--- arrayproblem.js
+++ arrayproblem.js
@@ -1,6 +1,20 @@
-//Problem: Check if "banana" exists in the array.
+// //push() add element in the last of the array. 
 
-const fruits=["banana","apple","grapes"]
-if(fruits.includes("banana")){
-    console.log("the bna is exsist in the array")
-}
+//  const fruits = ["apple", "banana"]
+//  fruits.push("orange")
+//  console.log(fruits)
+
+//  //----------------------------------------------------------------------------------------------------------------------
+
+//  //shift() remove the first element of the array.
+
+//  const books=["islamiyat","geograpghy","history","computer"]
+//  books.shift()
+//  console.log(books)
+
+//---------------------------------------------------------------------------------------------------------------------- 
+
+//toString() converts string into an array
+const players=["babar","azam","ali","raza","azhar","khan"]
+const more=players.toString()
+console.log(more)
---
+++ switch.js
@@ -0,0 +1,78 @@
+// the js evaluate a expression and execute block of code based on matching each case
+//syntax:-
+
+//  switch (expression){
+//     case value:
+
+//     case value:
+
+//     case value:
+
+// default;    
+
+//  }
+
+//Switch statement is evaluated once.
+//The value of expression compare with the value of each case.
+//if there is a match the associate block of code is executed
+//if there is no match, the deafult code of block executed
+
+// NOTE: code block to be execute if expression value match
+// code to be executed if no value match
+
+// let player=4;
+
+// switch (player) {
+//     case 1:
+//            player="babar azam";
+//         break;
+//     case 2:
+//            player="kohli";
+//         break;  
+//     case 3:
+//            player="rizwan";
+//         break; 
+//     case 4:
+//            player="shaheen"; 
+//         break;  
+//     case 5:
+//            player="haris";        
+
+//     default:
+//            player="nawaz"
+          
+//         break;
+// }
+
+// console.log(player)
+
+
+// let grade="c";
+// let result;
+
+// switch (grade) {
+//     case 'A': 
+//         result= 'grade of student ali';
+//         break;
+
+//     case 'b': 
+//         result= 'grade of student ahmed';
+//         break;
+    
+//     case 'c':    
+//         result='grade of student faizan';
+//         break;
+    
+//     case 'd':
+//         result='grade of student junaid'; 
+//         break;
+    
+//     case 'e':
+//         result='grade of student raza';
+//         break;
+
+//     default:
+//        result="results of student";   
+//        break;  
+// }
+// console.log(result);
